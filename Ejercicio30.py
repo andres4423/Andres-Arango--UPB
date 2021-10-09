40bc72ce66ab6ace580d2f3d456555d9e7901c00
andres4423 <79811184+andres4423@users.noreply.github.com>
2021-10-08 20:07:21 -0500

    Delete Ejercicio30.py

--- Ejercicio30.py
+++
@@ -1,8 +0,0 @@
-num1=int(input("Dame un numero"))
-num2=int(input("Dame un numero"))
-num3=int(input("Dame un numero"))
-suma=num1+num2
-if suma>num3:
-    print(suma,"Es mayor a",num3)
-else:
-    print(num3,"Es mayor a",suma)
