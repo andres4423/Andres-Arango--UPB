73f2753a05ccc5f9ae053ab700246f144109efaf
andres4423 <79811184+andres4423@users.noreply.github.com>
2021-10-08 20:07:34 -0500

    Delete Ejercicio34.py

--- Ejercicio34.py
+++
@@ -1,9 +0,0 @@
-usuario="Andres"
-contraseña="4423"
-
-user=input("Ingrese el usuario: ")
-contra=input("Ingrese la contraseña: ")
-if user==usuario and contra==contraseña:
-    print("Acceso aceptado")
-else:
-    print("Acceso denegado")
