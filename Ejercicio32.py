59188c1b57565e597b159b5ff6742220818d3c24
andres4423 <79811184+andres4423@users.noreply.github.com>
2021-10-08 20:07:27 -0500

    Delete Ejercicio32.py

--- Ejercicio32.py
+++
@@ -1,9 +0,0 @@
-num=int(input("Introduce un año: "))
-if num%4==0 and num%100==0:
-    print(num,"Es un año bisiesto")
-else:
-    print("No es bisiesto")
-if num%400==0:
-    print(num,"Es año bisiesto")
-else:
-    print(num,"No es un año bisiesto")
