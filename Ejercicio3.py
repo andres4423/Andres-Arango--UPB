7f930dc309dfeb96dfc4302a9bf98b5dfb50bca7
andres4423 <79811184+andres4423@users.noreply.github.com>
2021-10-08 20:04:16 -0500

    Add files via upload

---
+++ Ejercicio3.py
@@ -0,0 +1,3 @@
+num1=int(input("Dame un numero"))
+num2=int(input("Dame otro numero"))
+print(num1+num2)
---
+++ Ejercicio9.py
@@ -0,0 +1,5 @@
+num1=int(input("Dame 1 numero"))
+num2=int(input("Dame otro numero"))
+num3=int(input("dame otro numero"))
+promedio= num1+num2+num3/3
+print(promedio)
